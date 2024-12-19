edf68614f8bf2b556635b8579a51a13a3daed863
Paul Mourer <paul.mourer@onxmaps.com>
2024-12-18 20:34:05 -0700

    fix drawings, add a cancel tbd if it works

--- gen-images.sh
+++ gen-images.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-keymap -c ./keymap_drawer.config.yaml parse -z ./config/lily58.keymap > draw/lily58.yml &&\
-keymap -c ./keymap_drawer.config.yaml draw draw/lily58.yml -k "lily58/rev1" > ./draw/lily58.svg
+keymap -c ./keymap_drawer.config.yaml parse -z ./config/lily58.keymap > draw/lily58.yaml &&\
+keymap -c ./keymap_drawer.config.yaml draw draw/lily58.yaml -k "lily58/rev1" > ./draw/lily58.svg
 
-keymap -c ./keymap_drawer.config.yaml parse -z ./config/corne.keymap > draw/corne.yml
-keymap -c ./keymap_drawer.config.yaml draw draw/corne.yml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" > ./draw/corne.svg
+keymap -c ./keymap_drawer.config.yaml parse -z ./config/corne.keymap > draw/corne.yaml
+keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" > ./draw/corne.svg
