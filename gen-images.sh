319077140e3ae6350771f18b47a3d25111d707f7
pm0u <pm0u@users.noreply.github.com>
2024-12-21 17:27:30 -0700

    more lost changes

--- gen-images.sh
+++ gen-images.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-keymap -c ./keymap_drawer.config.yaml parse -z ./config/lily58.keymap > draw/lily58.yaml &&\
-keymap -c ./keymap_drawer.config.yaml draw draw/lily58.yaml -k "lily58/rev1" > ./draw/lily58.svg
-
 keymap -c ./keymap_drawer.config.yaml parse -z ./config/corne.keymap > draw/corne.yaml
-keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" > ./draw/corne.svg
+keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" --keys-only > ./draw/corne.svg
+keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" -s base --combos-only> ./draw/combos.svg
